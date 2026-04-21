ba0cef29a79653de4415a251030f5a49ba58c93b
to-mo-su-ke <magumarashi.247@gmail.com>
2026-04-21 13:48:29 +0900

    データ読込

---
+++ ex1/y_arakawa/ex1-1.py
@@ -0,0 +1,8 @@
+import numpy as np, pandas as pd
+
+
+# CSVファイルからデータを読み込む
+sample2d_1 = pd.read_csv("../data/sample2d_1.csv")
+sample2d_2 = pd.read_csv("../data/sample2d_2.csv")
+sample3d = pd.read_csv("../data/sample3d.csv")
+sample_logistic = pd.read_csv("../data/sample_logistic.csv")
