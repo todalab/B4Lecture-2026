f918d994598a15d6fa593eb784127c654b933c52
Toto <toto@chunghwa.asia>
2026-04-22 07:58:12 +0900

    Fix imports

--- ex1/j_to/main.py
+++ ex1/j_to/main.py
@@ -5,10 +5,10 @@ from typing import Callable
 
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
+import numpy as np
 from numpy import column_stack, linspace, meshgrid, ones_like
 from numpy.linalg import inv, norm
 from pandas import read_csv
-from pandas.core.reshape.tile import np
 
 
 def regression(
