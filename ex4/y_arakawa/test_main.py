48cae3a049433d88d2fa605d88fe56855ae2f3f1
to-mo-su-ke <magumarashi.247@gmail.com>
2026-05-19 14:26:05 +0900

    コードの書式を修正2

--- ex4/y_arakawa/test_main.py
+++ ex4/y_arakawa/test_main.py
@@ -3,13 +3,15 @@
 
 import pickle
 
-from main import (calculate_forward_likelihood,
-                  evaluate_model_selection_by_forward_algorithm,
-                  evaluate_model_selection_by_viterbi_algorithm,
-                  forward_algorithm,
-                  run_forward_algorithm_against_all_output_series,
-                  run_viterbi_algorithm_against_all_output_series,
-                  viterbi_algorithm)
+from main import (
+    calculate_forward_likelihood,
+    evaluate_model_selection_by_forward_algorithm,
+    evaluate_model_selection_by_viterbi_algorithm,
+    forward_algorithm,
+    run_forward_algorithm_against_all_output_series,
+    run_viterbi_algorithm_against_all_output_series,
+    viterbi_algorithm,
+)
 
 
 def test_forward_algorithm():
